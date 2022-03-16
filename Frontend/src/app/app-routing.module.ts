20ca7d0b539306c8cfc8bd15d37310ec24125a81
nikita <n.predein@profitbase.ru>
2022-03-16 22:40:45 +0500

    fix

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntranceComponent } from './components/entrance/entrance.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "entrance", component: EntranceComponent },
+  { path: 'devices', loadChildren: () => import('./modules/devices/devices.module').then(m => m.DevicesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntranceComponent } from './components/entrance/entrance.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntranceComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
