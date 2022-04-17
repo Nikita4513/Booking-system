25763940869c420fe000b41173837bb992b570c3
nikita <n.predein@profitbase.ru>
2022-04-17 15:38:35 +0500

    init component for book

--- Frontend/src/app/modules/account/modules/booking/components/book/book.component.ts
+++ Frontend/src/app/modules/account/modules/booking/components/book/book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IDevice } from 'src/app/modules/account/models/interfaces';
+import { DevicesService } from 'src/app/modules/account/services/devices.service';
 
 @Component({
   selector: 'app-book-device',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDeviceComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  device: IDevice = { name: "", id: this.id, year: 0, description: '', bookings: [] };
+
+  constructor(
+    private route: ActivatedRoute,
+    private devicesService: DevicesService
+  ) {
+   }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.devicesService.getDeviceById(this.id).pipe()
+      .subscribe(device => {
+        this.device = device;
+      })
   }
 
+  get today(): string {
+    return new Date().toISOString().split("T")[0];
+  }
 }
