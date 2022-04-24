679b80c32ee1f823afcc77fbe3d1da01934430ad
Nikita4513 <predein697@gmail.com>
2022-04-24 20:12:28 +0500

    refactoring

--- Frontend/src/app/modules/entrance/entrance-routing.module.ts
+++ Frontend/src/app/modules/entrance/entrance-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntranceComponent } from './entrance.component';
 import { FormEntranceComponent } from './form-entrance/form-entrance.component';
 
 const routes: Routes = [
-  { path: '', component: FormEntranceComponent}
+  { path: '', component: EntranceComponent}
 ];
 
 @NgModule({
